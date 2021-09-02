97f55178956d9fea86f6187e6bb6e0ffb12c41ea
Nirmal27032000 <60871011+Nirmal27032000@users.noreply.github.com>
2021-09-02 15:18:03 +0530

    "File uploded initial commit to math.py"

---
+++ math.py
@@ -0,0 +1,18 @@
+# ADD IMPLEMENTATION
+def add(a,b):
+   return a+b
+# Sub IMplemetaion
+def Sub(a,b): # for master branch 
+  return a-b
+# mul implementation
+def mul(a,b): # FOR BUG4 Branch 
+   return x*y
+# Divide implemetation
+def divide(a,b):
+   if a==0:
+      return error 
+   else:
+      a//b
+#square implemntaion 
+def square(a):
+   return a*a
